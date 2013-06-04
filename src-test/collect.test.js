ae09978a97c35d1fcddf201d33cfe68c117f2547
Paul Grenier <pgrenier@gmail.com>
2013-06-03 23:36:42 -0400

    added some tests

--- src-test/collect.test.js
+++ src-test/collect.test.js
@@ -1,12 +1,18 @@
 var data = {
     a: "test",
-    b: [1,2,3]
+    b: [1,2,3],
+    c: [{prop: "a"}, {}, {prop: "c"}]
 };
 
 collect.install();
 module("collect");
 test("from returns an array", function () {
     ok(Array.isArray(from(data)("a")()));
+});
+test("collect returns an empty array if from is not a function", function () {
+    var results = collect();
+    ok(Array.isArray(results));
+    ok(results.length === 0);
 });
 test("from returns a copy of an array", function () {
     ok(Array.isArray(from(data)("b")()));
@@ -35,4 +41,21 @@ test("collect filters results (DSL2)", function () {
     var query = first(tweet)(withId(122078461840982016));
     ok(query().length === 1);
     ok(query()[0].id === 122078461840982016);
+});
+test("deep eq", function () {
+    ok(collect(from(twitter_data)("results"), eq("metadata.result_type")("popular")).length === 3);
+});
+test("having tests existence", function () {
+    var results = collect(from(data)("c"), having("prop"));
+    ok(results.length === 2);
+});
+test("orderby creates a function used to derive values to sort by", function () {
+    var sorted = partial(collect)(3)(orderBy(function (item) {
+        return new Date(item.created_at);
+    }));
+    var results = sorted(from(twitter_data)("results"))();
+    ok(results[0].id === 122078461840982016);
+});
+test("deep having collect", function () {
+    ok(collect(from(twitter_data)("results"), having("entities.urls")).length === 3);
 });
--- src/collect.js
+++ src/collect.js
@@ -1,10 +1,12 @@
 (function (global) {
+    /**
+     * shim isArray
+     */
     if(!Array.isArray) {
         Array.isArray = function (vArg) {
             return Object.prototype.toString.call(vArg) === "[object Array]";
         };
     }
-
     /**
      * memoizes a single primitive argument
      * @param func
@@ -17,18 +19,27 @@
         };
     }
 
+    /**
+     * reaplces the first undefined parameter with val
+     * @param val
+     * @returns {Function}
+     */
     function fill(val) {
         return function (arr) {
             var copy = Array.prototype.slice.call(arr, 0);
             for (var i = 0, len = copy.length + 1; i < len; i += 1) {
-                if (!copy[i] && typeof copy[i] === "undefined") {
+                if (typeof copy[i] === "undefined") {
                     copy[i] = val;
                     return copy;
                 }
             }
         };
     }
-
+    /**
+     * creates a function to which bound arguments may curry further arguments
+     * @param fn
+     * @returns {Function}
+     */
     function partialApply(fn) {
         return function apply(args) {
             return function curry(arg) {
@@ -39,7 +50,10 @@
             };
         };
     }
-
+    /**
+     * depth-first approach to property access
+     * @type {Function}
+     */
     var getDepth = memoize(function (depth) {
         var params = "";
         var vars = "  var _0 = this";
@@ -60,10 +74,21 @@
         body = "  return " + (!body ? "_0;" : body + " : void(0);");
         return Function(params, vars + body);
     }),
-    getPath = memoize(function (path) {
+    /**
+     * splits string representations of paths into segments
+     * @param path
+     */
+    getSegments = memoize(function (path) {
         return path ? path.split ? path.split(".") : path : [];
     }),
     api = {
+        /**
+         * @param from
+         * @param where
+         * @param limit
+         * @param transform
+         * @returns {Array}
+         */
         collect: function (from, where, limit, transform) {
             var item;
             var results = [];
@@ -84,6 +109,10 @@
             }
             return results;
         },
+        /**
+         * @param fn
+         * @returns {Function}
+         */
         partial: function (fn) {
             return function (pos) {
                 return function (arg) {
@@ -93,23 +122,39 @@
                 };
             };
         },
+        /**
+         * @param quantity
+         * @returns {Function}
+         */
         limitTo: function (quantity) {
             return function (item, results) {
                 return results.length === quantity;
             };
         },
-        has: function (prop) {
+        /**
+         * @param path
+         * @returns {Function}
+         */
+        having: function (path) {
             return function (obj) {
-                return prop in obj;
+                return !!(obj && get(path, obj));
             }
         },
-        eq: function (prop) {
+        /**
+         * @param path
+         * @returns {Function}
+         */
+        eq: function (path) {
             return function (val) {
                 return function (obj) {
-                    return obj[prop] === val;
+                    return !!(obj && get(path, obj) === val);
                 };
             };
         },
+        /**
+         * @param getVal
+         * @returns {Function}
+         */
         orderBy: function (getVal) {
             return function (arr) {
                 return arr.sort(function (a, b) {
@@ -119,23 +164,31 @@
                 });
             };
         },
+        /**
+         * @param path
+         * @param obj
+         * @returns {*}
+         */
         get: function (path, obj) {
-            var params = getPath(path);
+            var params = getSegments(path);
             return getDepth(params.length).apply(obj || this, params);
         },
+        /**
+         * takes an object then returns a function which takes a string
+         * @returns {Function}
+         */
         from: function () {
             return partialApply(function (obj, path) {
                 var val = get(path, obj);
                 return Array.isArray(val) ? val.slice(0) : [val];
             })(arguments);
-        },
-        makeNumber: function (prop) {
-            return function (obj) {
-                return Number(obj[prop]) || 0;
-            };
         }
     };
     global.collect = {
+        /**
+         * installs the collect api to an object or global
+         * @param obj
+         */
         install: function (obj) {
             var target = obj || global, key;
             for (key in api) {
